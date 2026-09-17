9e18db9ca2bcc4a4c1ab4db901bbeef8da7b21f6
Pistachopower <nelson.galicia.carrero.al@iespoligonosur.org>
2026-09-17 19:06:59 +0200

    Configura despliegue en GitHub Pages

--- tienda/vite.config.js
+++ tienda/vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  base: '/Proyecto_Diseno_Web/',
   resolve: {
     alias: {
       '@': '/src'
